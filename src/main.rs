b1259718c7e6e32ce0a4568d0520e38cf1790075
0xViolette <violettether@gmail.com>
2026-07-13 17:20:24 +0530

    read input and parse commands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+struct ParsedInput<'a> {
+    command: &'a str,
+    options: Option<Vec<&'a str>>,
+}
+
+fn parse_input<'a>(input: &'a str) -> ParsedInput<'a> {
+    ParsedInput {
+        command: input.trim_end(),
+        options: None,
+    }
+}
+
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Could not read the command");
+
+    let parsed_input = parse_input(&input.as_str());
+
+    println!("{}: command not found", parsed_input.command);
 }
